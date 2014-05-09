a0a5aa0d360d7987d2defbe1f1ab047ffa56e81d
LevenBravo <charles.j.crow@gmail.com>
2014-05-09 13:38:22 -0700

    Create Kieth.hpp

---
+++ Kieth.hpp
@@ -0,0 +1,21 @@
+/*
+ CSE 220 EC
+ 
+ Charles Crow (LevenBravo)
+ Mike Mayr (MMayr15)
+ 
+ */
+
+#ifdef KEITH_H
+#define KEITH_H
+
+#include <iostream>
+#include <math.h>
+
+#include "Types.h"
+
+extern void FindKeiths(ulong pLimit);
+
+extern void *FindKeithsThread(void *pState);
+
+#endif
