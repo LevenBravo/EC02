2736f36b44bd9e343aa605e7d723020a4c9d8884
LevenBravo <charles.j.crow@gmail.com>
2014-05-09 23:44:26 -0700

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -14,9 +14,9 @@ using std::endl;
 // Static Function Declarations
 //==============================================================================================================
 static bool IsPrime
-    (
-    ulong const pNum
-    );
+(
+ ulong const pNum
+ );
 
 //==============================================================================================================
 // Function Definitions
@@ -28,9 +28,9 @@ static bool IsPrime
 // Finds all prime numbers from 2 to pLimit. We do nothing with the numbers we find.
 //--------------------------------------------------------------------------------------------------------------
 void FindPrimes
-    (
-    ulong pLimit
-    )
+(
+ ulong pLimit
+ )
 {
     if (GetVerbose()) cout << "FindPrimes() Begin" << endl;
     for (ulong n = 2L; n <= pLimit; n += 2L) {
@@ -46,25 +46,25 @@ void FindPrimes
 // A thread which finds all prime numbers from 2 to pState->mLimit.
 //--------------------------------------------------------------------------------------------------------------
 void *FindPrimesThread
-    (
-    void *pState
-    )
+(
+ void *pState
+ )
 {
     // If running in verbose mode, then tell the user that the thread is beginning.
     if (GetVerbose()) cout << "FindPrimesThread() Begin" << endl;
-
+    
     // We know that pState is really a pointer to a ThreadState structure, so type cast it.
     ThreadState *state = static_cast<ThreadState *>(pState);
-
+    
     // Find all the primes from 2 to state->mLimit.
     FindPrimes(state->mLimit);
-
+    
     // The thread exit code is 0 (success).
     state->mExitCode = 0;
-
+    
     // If running verbose mode, then tell the user that the thread is ending.
     if (GetVerbose()) cout << "FindPrimesThread() End" << endl;
-
+    
     // Exit (terminate) the thread and return pState as the return variable.
     pthread_exit(pState);
 }
@@ -76,9 +76,9 @@ void *FindPrimesThread
 // that's not the point of this project.
 //--------------------------------------------------------------------------------------------------------------
 static bool IsPrime
-    (
-    ulong const pNum
-    )
+(
+ ulong const pNum
+ )
 {
     if (pNum < 2L) return false;
     if (pNum == 2L) return true;
